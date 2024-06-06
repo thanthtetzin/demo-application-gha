41df7d69bf603ccd72830ab76b6e906426aa70ef
firatstory.eth <talefirat@gmail.com>
2024-06-06 10:02:22 +0200

    test: :white_check_mark: add vitest

---
+++ frontend/tests/sample.test.ts
@@ -0,0 +1,9 @@
+// sum.test.js
+import { expect, it, describe } from 'vitest';
+
+describe('Some sample tests', () => {
+	it('should sum numbers', async () => {
+		const result = 1 + 2;
+		expect(result).toBe(3);
+	});
+});
