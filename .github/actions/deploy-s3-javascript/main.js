c4b074152c47f497ed9a454cd93853eb36eeb241
firatstory.eth <talefirat@gmail.com>
2024-06-06 16:43:43 +0200

    add main.js

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,8 @@
+import core from '@actions/core';
+// import github from '@actions/github';
+// import exec from '@actions/exec';
+function run() {
+	core.notice('Hello from my Custom Action');
+}
+
+run();
