c5dbfaea57fab196f91cfa75ddade6bf4dd84540
firatstory.eth <talefirat@gmail.com>
2024-06-06 17:37:11 +0200

    auto deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,16 @@
 // eslint-disable-next-line no-undef
 const core = require('@actions/core');
 // import github from '@actions/github';
-// import exec from '@actions/exec';
+import exec from '@actions/exec';
 function run() {
 	// 1 - Get the inputs
+	const bucket = core.getInput('bucket', { required: true });
+	const bucketRegion = core.getInput('bucket-region', { required: false });
+	const distFolder = core.getInput('dist-folder', { required: false });
+
+	// 2- Upload My Files to S3
+
+	exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
 	core.notice('Hello from my Custom Action');
 }
 
