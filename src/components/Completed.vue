7c98555e4405310b9d190e094e35fcfdf9ca8127
firatstory.eth <talefirat@gmail.com>
2024-06-06 14:25:31 +0200

    update readme

--- src/components/Completed.vue
+++ src/components/Completed.vue
@@ -1,38 +1,30 @@
 <script setup lang="ts">
-import { FwbCard, FwbHeading, FwbCheckbox } from "flowbite-vue";
-import { ref, onMounted } from "vue";
+import { FwbCard, FwbHeading, FwbCheckbox } from 'flowbite-vue';
+import { ref, onMounted } from 'vue';
 
-import { getCompleted, type Task } from "../services/todo";
+import { getCompleted, type Task } from '../services/todo';
 
 const todos = ref<Task[]>([]);
 
 onMounted(async () => {
-  todos.value = await getCompleted();
+	todos.value = await getCompleted();
 });
 </script>
 <template>
-  <fwb-card class="p-5 max-w- w-full hover:bg-white">
-    <FwbHeading class="mb-8">Completed</FwbHeading>
+	<fwb-card class="p-5 max-w- w-full hover:bg-white">
+		<FwbHeading class="mb-8">Completed</FwbHeading>
 
-    <div class="flex flex-col gap-2">
-      <div
-        v-for="task in todos"
-        :class="`todo-item ${task.isDone && 'done'}`"
-        class="flex gap-2"
-      >
-        <FwbCheckbox v-model="task.isDone" disabled />
-        <div>
-          {{ task.title }}
-        </div>
-        <p>
-          <b>Completed:</b>
-          {{
-            new Intl.DateTimeFormat("default", { dateStyle: "short" }).format(
-              new Date(task.updatedAt!)
-            )
-          }}
-        </p>
-      </div>
-    </div>
-  </fwb-card>
+		<div class="flex flex-col gap-2">
+			<div v-for="task in todos" :class="`todo-item ${task.isDone && 'done'}`" class="flex gap-2">
+				<FwbCheckbox v-model="task.isDone" disabled />
+				<div>
+					{{ task.title }}
+				</div>
+				<p>
+					<b>Completed:</b>
+					{{ new Intl.DateTimeFormat('default', { dateStyle: 'short' }).format(new Date(task.updatedAt!)) }}
+				</p>
+			</div>
+		</div>
+	</fwb-card>
 </template>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,88 +1,72 @@
 <script setup lang="ts">
-import {
-  FwbCard,
-  FwbHeading,
-  FwbInput,
-  FwbButton,
-  FwbCheckbox,
-} from "flowbite-vue";
-import { ref, onMounted } from "vue";
+import { FwbCard, FwbHeading, FwbInput, FwbButton, FwbCheckbox } from 'flowbite-vue';
+import { ref, onMounted } from 'vue';
 
-import {
-  getTodos,
-  addTodo,
-  deleteTodo,
-  updateTodo,
-  type Task,
-} from "../services/todo";
+import { getTodos, addTodo, deleteTodo, updateTodo, type Task } from '../services/todo';
 
 const todos = ref<Task[]>([]);
-const text = ref("");
+const text = ref('');
 
 async function submit() {
-  if (text.value.trim() === "") {
-    return;
-  }
+	if (text.value.trim() === '') {
+		return;
+	}
 
-  todos.value.unshift(
-    await addTodo({
-      title: text.value,
-      isDone: false,
-    })
-  );
+	todos.value.unshift(
+		await addTodo({
+			title: text.value,
+			isDone: false,
+		})
+	);
 
-  text.value = "";
+	text.value = '';
 }
 
 async function remove(todo: Task) {
-  todos.value = todos.value.filter((x) => x !== todo);
-  await deleteTodo(todo);
+	todos.value = todos.value.filter((x) => x !== todo);
+	await deleteTodo(todo);
 }
 
 async function update(todo: Task) {
-  updateTodo(todo);
+	updateTodo(todo);
 }
 
 onMounted(() => {
-  getTodos().then((response) => (todos.value = response));
+	getTodos().then((response) => (todos.value = response));
 });
 </script>
 <template>
-  <fwb-card class="p-5 max-w-md w-full hover:bg-white">
-    <FwbHeading class="mb-8">My Todos</FwbHeading>
+	<fwb-card class="p-5 max-w-md w-full hover:bg-white">
+		<FwbHeading class="mb-8">My Todos</FwbHeading>
 
-    <div class="mb-8">
-      <form @submit.prevent="submit">
-        <FwbInput
-          type="text"
-          placeholder="e.g. email your boss"
-          v-model="text"
-          label="What do you plan on doing? 🙂"
-          class="mb-2"
-        />
+		<div class="mb-8">
+			<form @submit.prevent="submit">
+				<FwbInput
+					type="text"
+					placeholder="e.g. email your boss"
+					v-model="text"
+					label="What do you plan on doing? 🙂"
+					class="mb-2"
+				/>
 
-        <FwbButton type="submit"> Add todo </FwbButton>
-      </form>
-    </div>
+				<FwbButton type="submit"> Add todo </FwbButton>
+			</form>
+		</div>
 
-    <div v-if="todos.length === 0">
-      <strong>No Todos Here 🎉</strong>
-      <p>Either you are super productive or lazy to add tasks.</p>
-    </div>
+		<div v-if="todos.length === 0">
+			<strong>No Todos Here 🎉</strong>
+			<p>Either you are super productive or lazy to add tasks.</p>
+		</div>
 
-    <div class="flex flex-col gap-2">
-      <div
-        v-for="task in todos"
-        :class="`todo-item ${task.isDone && 'done'}`"
-        class="flex gap-2"
-      >
-        <FwbCheckbox v-model="task.isDone" @change="update(task)" />
-        <FwbInput class="grow" type="text" v-model="task.title">
-          <template #suffix>
-            <button class="delete" @click="remove(task)">Delete</button>
-          </template>
-        </FwbInput>
-      </div>
-    </div>
-  </fwb-card>
+		<div class="flex flex-col gap-2">
+			<div v-for="task in todos" :class="`todo-item ${task.isDone && 'done'}`" class="flex gap-2">
+				<FwbCheckbox v-model="task.isDone" @change="update(task)" />
+				<FwbInput class="grow" type="text" v-model="task.title">
+					<template #suffix>
+						<button class="delete" @click="remove(task)">Delete</button>
+					</template>
+				</FwbInput>
+			</div>
+		</div>
+	</fwb-card>
 </template>
