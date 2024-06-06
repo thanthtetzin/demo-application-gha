74ebc152eaef895944852b3ebacfe1f2a22427c4
firatstory.eth <talefirat@gmail.com>
2024-06-06 17:11:43 +0200

    remove nodemodules

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 // import github from '@actions/github';
 // import exec from '@actions/exec';
 function run() {
+	// 1 - Get the inputs
 	core.notice('Hello from my Custom Action');
 }
 
