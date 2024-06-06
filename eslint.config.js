3c4362d13dd34dcd7e800fc9d8d39a40a1236535
firatstory.eth <talefirat@gmail.com>
2024-06-06 14:35:46 +0200

    comment out linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,12 @@
-import pluginVue from 'eslint-plugin-vue';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+
+
 export default [
-	// add more generic rulesets here, such as:
-	// js.configs.recommended,
-	...pluginVue.configs['flat/recommended'],
-	// ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
-	{
-		rules: {
-			// override/add rules settings here, such as:
-			// 'vue/no-unused-vars': 'error'
-		},
-	},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
 ];
